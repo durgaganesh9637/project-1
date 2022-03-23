a2c697ec05af4a258aff12d05243302c25e1a815
durgaganesh9637 <99676335+durgaganesh9637@users.noreply.github.com>
2022-03-23 14:17:52 +0530

    Add files via upload

---
+++ creds.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    access_key = "ASIAT5PRFJ63KJYHQLKO"
+    secret_key = "0rZkLoGI80MdWh+dm07mY4PqzzkaWMvirXnqXqVa"
+    token   =   "FwoGZXIvYXdzELv//////////wEaDNb0i6ygPy0riMQ2yiK+AdtK/vcpH/yhHReeKD39OIJiniSLAa0qA68Zh3Ix1lKScSXfQwMTX0dSsI7H+abDDGKy1Se0Rt/4sKU5zWwNaRL6jK/f4Jx5GPiPy8gL3n9qm2PtuPsvwQ+i95pVMrSOHnbkyEKFfONAPmJbqm18VFN4wF8gGkanu00jreIz3pUqoHuVcbRrXxOwzr+7RasSO92wm76KfWxj1DuERXYCvVOf+9A4RFwmQlK5Edr1BMdkzbb0R0zzV8wEX/6NpPUo8JDokQYyLV2j3UxCeu854BjLGYxz9BE/7+rOnwd2ghqbEWo3lSaFiJokUIOMt/l4ST3O7Q=="
+    region = "us-east-1"
+}
