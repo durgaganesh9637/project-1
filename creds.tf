acf32f7b5714d1d3674780165d6fc45a01373b28
durgaganesh9637 <99676335+durgaganesh9637@users.noreply.github.com>
2022-03-24 12:03:36 +0530

    Update creds.tf

--- creds.tf
+++ creds.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "ASIAT5PRFJ63KJYHQLKO"
-    secret_key = "0rZkLoGI80MdWh+dm07mY4PqzzkaWMvirXnqXqVa"
-    token   =   "FwoGZXIvYXdzELv//////////wEaDNb0i6ygPy0riMQ2yiK+AdtK/vcpH/yhHReeKD39OIJiniSLAa0qA68Zh3Ix1lKScSXfQwMTX0dSsI7H+abDDGKy1Se0Rt/4sKU5zWwNaRL6jK/f4Jx5GPiPy8gL3n9qm2PtuPsvwQ+i95pVMrSOHnbkyEKFfONAPmJbqm18VFN4wF8gGkanu00jreIz3pUqoHuVcbRrXxOwzr+7RasSO92wm76KfWxj1DuERXYCvVOf+9A4RFwmQlK5Edr1BMdkzbb0R0zzV8wEX/6NpPUo8JDokQYyLV2j3UxCeu854BjLGYxz9BE/7+rOnwd2ghqbEWo3lSaFiJokUIOMt/l4ST3O7Q=="
+    access_key = "ASIAT5PRFJ63F5T752SC"
+    secret_key = "/K9t+ZH7j+KQUcw8ZVL2uCcAwd75CrpeJzJXIkY+"
+    token   =   "FwoGZXIvYXdzEN///////////wEaDKJtbzrBcjmzEpj/KSK+AQaMhp+z+T2n9SsozmqhCwpj0g84fTaimE4fiG0FsZ283mVZd6djkCmyVDhCTW83/imREFNIwlmX3uHf2fz/m8a2XDle93SEWB4YnHRejeD1Oeppa1Mk607hkUgVCFn7mtejOggI1kVNZ5nTPmK0Cq10C3QxuKpGP7xeWlJmq9UT0FBikxCLKVowwO3VXqgJbfZtumV+sI2qnqMngHfvDLtwMmxemOfsUTI+SobEcninIMEnYUHGmhWeZBJHOLgo8Y7wkQYyLWw5LNvqtblrd5IlspPRPMynz5ESWLLFuqAqTK8td8IwAYAJMbSv/pxKvAQm/A=="
     region = "us-east-1"
 }
