f0c5f8db8799a5da94585d6efab9d1aa441e3e2b
durgaganesh9637 <99676335+durgaganesh9637@users.noreply.github.com>
2022-06-05 16:13:04 +0530

    Update creds.tf

--- creds.tf
+++ creds.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "ASIAT5PRFJ63F5T752SC"
-    secret_key = "/K9t+ZH7j+KQUcw8ZVL2uCcAwd75CrpeJzJXIkY+"
+    access_key = "ASIAT5PRFJ63F5C"
+    secret_key = "/K9t+ZH7j+KQUcw8ZpeJzJXIkY+"
     token   =   "FwoGZXIvYXdzEN///////////wEaDKJtbzrBcjmzEpj/KSK+AQaMhp+z+T2n9SsozmqhCwpj0g84fTaimE4fiG0FsZ283mVZd6djkCmyVDhCTW83/imREFNIwlmX3uHf2fz/m8a2XDle93SEWB4YnHRejeD1Oeppa1Mk607hkUgVCFn7mtejOggI1kVNZ5nTPmK0Cq10C3QxuKpGP7xeWlJmq9UT0FBikxCLKVowwO3VXqgJbfZtumV+sI2qnqMngHfvDLtwMmxemOfsUTI+SobEcninIMEnYUHGmhWeZBJHOLgo8Y7wkQYyLWw5LNvqtblrd5IlspPRPMynz5ESWLLFuqAqTK8td8IwAYAJMbSv/pxKvAQm/A=="
     region = "us-east-1"
 }
